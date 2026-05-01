cfaf1256db5f57b0140b03680910190dfbbac60f
Vaibhav Annaso Tembukade <vaibhavtembukadea09@gmail.com>
2026-05-01 14:20:26 +0530

    feat: integrate Clerk for authentication and add sign-in/sign-up pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 justify-center items-center ">
-   <Button >Hello world</Button>
+   <UserButton />
   </div>
   );
 }
